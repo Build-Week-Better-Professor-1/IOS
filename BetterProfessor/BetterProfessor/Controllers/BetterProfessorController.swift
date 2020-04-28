3a4a66e647bb6332b13ee7e6f64a7d1e83372245
Lydia Zhang <art4everyan@gmail.com>
2020-04-28 15:55:31 -0400

    start on fetch

---
+++ BetterProfessor/BetterProfessor/Controllers/BetterProfessorController.swift
@@ -0,0 +1,19 @@
+//
+//  BetterProfessorController.swift
+//  BetterProfessor
+//
+//  Created by Lydia Zhang on 4/28/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+let baseUrl = URL(string: "https://betterprofessorapp.herokuapp.com/api")!
+
+class BetterProfessorController {
+    
+//    func fetchStudent(completion: @escaping ((Error?) -> Void) {
+//        
+//    }
+}
