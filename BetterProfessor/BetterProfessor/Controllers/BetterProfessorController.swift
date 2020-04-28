f9480cf93afc6fdc9e3c05144c0b7cd2f7c7f897
Lydia Zhang <art4everyan@gmail.com>
2020-04-28 16:19:46 -0400

    working on fetch

--- BetterProfessor/BetterProfessor/Controllers/BetterProfessorController.swift
+++ BetterProfessor/BetterProfessor/Controllers/BetterProfessorController.swift
@@ -10,10 +10,35 @@ import Foundation
 import CoreData
 
 let baseUrl = URL(string: "https://betterprofessorapp.herokuapp.com/api")!
+let studentURL = baseUrl.appendingPathComponent("/api/students")
 
 class BetterProfessorController {
     
-//    func fetchStudent(completion: @escaping ((Error?) -> Void) {
-//        
-//    }
+    func fetchStudent(completion: @escaping ((Error?) -> Void) = { _ in }) {
+        let requestURL = studentURL.appendingPathExtension("json")
+        
+        URLSession.shared.dataTask(with: requestURL) { data, _, error in
+            if let error = error {
+                NSLog("Error fetching entries from server: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned from data task")
+                completion(NSError())
+                return
+            }
+            
+            var studentRep: [StudentRepresentation]
+            
+            do {
+                //try JSONDecoder.decode([x, String: StudentRepresentation].self, from: data)
+            } catch {
+                
+            }
+        }
+    }
+    
+    
 }
