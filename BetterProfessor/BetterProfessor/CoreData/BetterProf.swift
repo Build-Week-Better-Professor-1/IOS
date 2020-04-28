182f575e401710190a145d20b1001ec5b26bf8f9
Lydia Zhang <art4everyan@gmail.com>
2020-04-28 10:53:29 -0400

    conv. file version 1

---
+++ BetterProfessor/BetterProfessor/CoreData/BetterProf.swift
@@ -0,0 +1,44 @@
+//
+//  BetterProf.swift
+//  BetterProfessor
+//
+//  Created by Lydia Zhang on 4/28/20.
+//  Copyright © 2020 Chris Dobek. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension User {
+    @discardableResult convenience init(username: String,
+        password: String,
+        context: NSManagedObjectContext = CoreDataStack.shared.mainContext
+    ) {
+    
+    self.init(context: context)
+    self.username = username
+    self.password = password
+    }
+}
+
+struct Bearer: Codable {
+    let token: String
+}
+
+extension Student {
+    @discardableResult convenience init(name: String,
+        email: String,
+        taskDueDate: Date?,
+        taskTitle: String,
+        taskNote: String?,
+        context: NSManagedObjectContext = CoreDataStack.shared.mainContext
+    ) {
+    
+    self.init(context: context)
+    self.name = name
+    self.email = email
+    self.taskNote = taskNote
+    self.taskTitle = taskTitle
+    self.taskTitle = taskTitle
+    }
+}
