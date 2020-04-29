e7f3630ef39ffbe7c9399825e6ae01e17dc006df
Lydia Zhang <art4everyan@gmail.com>
2020-04-29 14:55:13 -0400

    Starting Uinit test

--- BetterProfessor/BetterProfessorTests/BetterProfessorController.swift
+++ BetterProfessor/BetterProfessorTests/BetterProfessorController.swift
@@ -1,5 +1,5 @@
 //
-//  BetterProfessorTests.swift
+//  BetterProfessorController.swift
 //  BetterProfessorTests
 //
 //  Created by Lydia Zhang on 4/29/20.
@@ -7,8 +7,9 @@
 //
 
 import XCTest
+@testable import BetterProfessor
 
-class BetterProfessorTests: XCTestCase {
+class BetterProfessorController: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -25,7 +26,7 @@ class BetterProfessorTests: XCTestCase {
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
