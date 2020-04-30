bdd3950496ba73543655296cae8a75cb5785c18e
Lydia Zhang <art4everyan@gmail.com>
2020-04-30 15:59:49 -0400

    swiftlint

---
+++ BetterProfessor/Pods/Target Support Files/Pods-BetterProfessor/Pods-BetterProfessor-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BetterProfessor : NSObject
+@end
+@implementation PodsDummy_Pods_BetterProfessor
+@end
---
+++ BetterProfessor/Pods/Target Support Files/Pods-BetterProfessorTests/Pods-BetterProfessorTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BetterProfessorTests : NSObject
+@end
+@implementation PodsDummy_Pods_BetterProfessorTests
+@end
